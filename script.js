3a33c34ecf78672c49a0e715df46b379143c6353
anamariadanila <ana.danila@info.uaic.ro>
2022-06-15 15:19:32 +0300

    esc keypress event

--- script.js
+++ script.js
@@ -23,3 +23,12 @@ for (let i = 0; i < btnShowModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log('a key was pressed');
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
